b17a21e7c6a9221110b086e1f9da86621d0c290e
andregarros <andregarros222@gmail.com>
2024-07-01 17:44:08 -0300

    first commit

---
+++ js.js
@@ -0,0 +1,47 @@
+//priemira questão
+var nome = "andre"
+console.log(`seu nome é ${nome}` )
+
+//segunda questao
+const frutas = ["maçã", "banana", "morango"] //exibi e armazena valores
+console.log(frutas)
+//terceira questao
+const frutas1 = ["maçã", "banana", "morango"] //exibi e armazena valores
+console.log(frutas[2])
+//quarta quetsão
+var numero = 2
+console.log(`a soma da area é ${2 * 2}`)
+
+//quinta questao
+var a = 10
+var b = 5
+var c = 20
+var soma = a + b
+if(soma < 20){
+    console.log(`a = ${a} b = ${b} c = ${c}`)
+    console.log(`${a} + ${b} = ${soma} e menor que ${c}`)
+}
+//sexta questao
+var numero = '5'
+var soma = numero + 10
+console.log(`a soma é ${soma}`)
+
+//setima questao
+var nota1 = 10
+var nota2 = 10
+if(nota1 == nota2){
+    console.log(`a soma de ${nota1} + ${nota2} = ${nota1 + nota2}`)
+}
+else if(nota1 != nota2 ){
+    console.log(`a soma de ${nota1} x ${nota2} = ${nota1 * nota2}`)
+}
+//oitava questao
+for(let contador = 0; contador < 5; contador++){
+    console.log(contador)
+    //colocando o let não muda em nada,pois o var tbm é uma variavel
+}
+//nona questão
+for(let n = 1; n < 4; n++){
+    console.log(n)
+    //colocando o let não muda em nada,pois o var tbm é uma variavel
+}
---
+++ node_80fd3ffd5702f.tmp.js
@@ -0,0 +1,34 @@
+var nome = "andre"
+console.log(`seu nome é ${nome}` )
+
+const frutas = ["maçã", "banana", "morango"] //exibi e armazena valores
+console.log(frutas[0])
+for(let f = 0; f < frutas.length; f++){ //lenght significa tamanho do array
+    console.log(frutas[f])
+}
+
+var numero = 2
+console.log(`a soma da area é ${2 * 2}`)
+
+
+var a = 10
+var b = 5
+var c = 20
+var soma = a + b
+if(soma < 20){
+    console.log(`a = ${a} b = ${b} c = ${c}`)
+    console.log(`${a} + ${b} = ${soma} e menor que ${c}`)
+}
+
+var numero = '5'
+var soma = numero + 10
+console.log(`a soma é ${soma}`)
+
+var nota1 = 10
+var nota2 = 5
+if(a = b){
+    console.log(`a soma de ${nota1} + ${nota2} = ${nota1 + nota2}`)
+}
+else if(a != b ){
+    console.log(`a soma de ${nota1} x ${nota2} = ${nota1 * nota2}`)
+}
